0636895106c12fc9bf26762f0b06b629fc6881de
Premchand <mr.vprem99@gmail.com>
2023-08-27 17:21:57 +0530

    My first commit

---
+++ Test Suites/ts1.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?><FilteringTestSuiteEntity>
+   <description/>
+   <name>ts1</name>
+   <tag/>
+   <isRerun>false</isRerun>
+   <mailRecipient/>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>263905bc-18ca-418e-8c99-b608e752eaa8</testSuiteGuid>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension/>
+   <filteringPlugin/>
+   <filteringText>tag=(prem2) </filteringText>
+</FilteringTestSuiteEntity>
